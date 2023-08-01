ea38e46a7baf925b78a6b5b0792e86df069586e1
Iamneo <techadmin@iamneo.ai>
2023-08-01 11:20:46 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/Model.java
+++ springapp/src/main/java/com/examly/springapp/Model.java
@@ -8,46 +8,43 @@ import javax.persistence.Table;
 @Table
 public class Model {
     @Id
- @Column  
- private int taskId;
- @Column  
- private String taskHolderName;
- @Column
- private String taskDate;
- @Column
- private String taskName;
- @Column
- private String taskStatus;
- 
- public int getId() {
-    return id;
-}
-public void setId(int id) {
-    this.id = id;
-}
-public String getName() {
-    return name;
-}
-public void setName(String name) {
-    this.name = name;
-}
-public String getDate() {
-    return date;
-}
-public void setDate(String date) {
-    this.date = date;
-}
-public String getTaskName() {
-    return taskName;
-}
-public void setTaskName(String taskName) {
-    this.taskName = taskName;
-}
-public String getTaskStatus() {
-    return taskStatus;
-}
-public void setTaskStatus(String taskStatus) {
-    this.taskStatus = taskStatus;
-}
-
+    @Column  
+    private int taskId;
+    @Column
+    private String taskHolderName;
+    @Column
+    private String taskDate;
+    @Column
+    private String taskName;
+    @Column
+    private String taskStatus;
+    public int getTaskId() {
+        return taskId;
+    }
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+    public String getTaskHolderName() {
+        return taskHolderName;
+    }
+    public void setTaskHolderName(String taskHolderName) {
+        this.taskHolderName = taskHolderName;
+    }
+    public String getTaskDate() {
+        return taskDate;
+    }
+    public void setTaskDate(String taskDate) {
+        this.taskDate = taskDate;
+    }
+    public String getTaskName() {
+        return taskName;
+    }
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+    
+    
 }
