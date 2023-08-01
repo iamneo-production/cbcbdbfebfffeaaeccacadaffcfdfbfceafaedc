2c8183975e7ea1c1dbda3b7fe1ac54acb021886e
Iamneo <techadmin@iamneo.ai>
2023-08-01 13:22:38 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/AppController.java
+++ springapp/src/main/java/com/examly/springapp/AppController.java
@@ -1,15 +1,20 @@
 package com.examly.springapp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;  
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.*;
 @RestController
 public class AppController {
     @Autowired
     Dao dao;
+    private List<Model> all=(List<Model>) dao.findAll();
     @PostMapping("/saveTask")
     public String saveTask(@RequestBody Model detail) {
         dao.save(detail);
@@ -27,8 +32,10 @@ public class AppController {
         dao.deleteById(id);
     }
     @GetMapping("/getTask")
-    public Model getTask(@RequestParam int id){
+    public ResponseEntity<Model> getTask(@RequestParam("id") Integer id){
         Model a =dao.findById(id).orElse(null);
-        return a;
+        return ResponseEntity.ok(a);
 }
+
+
 }
