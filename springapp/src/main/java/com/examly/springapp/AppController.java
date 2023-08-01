6071937ce8e250e4778a6acc563f261202c4aba7
Iamneo <techadmin@iamneo.ai>
2023-08-01 10:33:02 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/examly/springapp/AppController.java
@@ -0,0 +1,11 @@
+package com.examly.springapp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;  
+
+public class AppController {
+    
+}
