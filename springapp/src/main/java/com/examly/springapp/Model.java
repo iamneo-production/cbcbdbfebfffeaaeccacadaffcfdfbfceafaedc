04ed62066af9e80cf041c894b2b5ac3f49e7d63a
Iamneo <techadmin@iamneo.ai>
2023-08-01 10:55:46 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/Model.java
+++ springapp/src/main/java/com/examly/springapp/Model.java
@@ -1,8 +1,8 @@
 package com.examly.springapp;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table
